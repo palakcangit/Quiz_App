0c7b5914bc0112715448c65d536996bd872eee7a
palakcangit <palaksharmas771@gmail.com>
2023-11-17 20:49:29 +0530

    complete create quiz endpoint with status auto decided by controller action

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -3,7 +3,7 @@ import { errorHandler } from "../utills/errorHandler.js";
 
 export const createQuiz = async (req, res, next) => {
   let { questions, startDate, endDate } = req.body;
-  if (!questions || !options || !rightAnswer || !startDate || !endDate) {
+  if (!questions || !startDate || !endDate) {
     errorHandler(
       "Send All Reuired Data:- questions, startDate, endDate ",
       400,
@@ -11,10 +11,26 @@ export const createQuiz = async (req, res, next) => {
     );
   } else {
     try {
+      let startTimeOfQuiz = new Date(startDate).toLocaleDateString();
+      let endTimeOfQuiz = new Date(endDate).toLocaleDateString();
+      let currentTime = new Date().setHours(0, 0, 0, 0);
+      currentTime = new Date(currentTime).toLocaleDateString();
+      let status;
+      if (startTimeOfQuiz > currentTime) {
+        status = "inactive";
+      } else if (endTimeOfQuiz < currentTime) {
+        status = "finished";
+      } else if (
+        currentTime >= startTimeOfQuiz &&
+        currentTime <= endTimeOfQuiz
+      ) {
+        status = "active";
+      }
       let quiz = await Quiz.create({
         questions,
         startDate,
         endDate,
+        status,
       });
       return res.status(200).json({
         success: true,
