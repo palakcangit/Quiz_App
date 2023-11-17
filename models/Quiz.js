a4cab4df5308c7f389aece06a086599314f09094
palakcangit <palaksharmas771@gmail.com>
2023-11-17 13:29:37 +0530

    design quiz schema

---
+++ models/Quiz.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate";
+
+const schema = new mongoose.Schema(
+  {
+    question: {
+      type: String,
+      required: true,
+    },
+    options: {
+      a: {
+        type: String,
+        required: true,
+      },
+      b: {
+        type: String,
+        required: true,
+      },
+      c: {
+        type: String,
+        required: true,
+      },
+      d: {
+        type: String,
+        required: true,
+      },
+    },
+    rightAnswer: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["inactive", "active", "finished"],
+    },
+  },
+  { timestamps: true }
+);
+
+schema.plugin(mongoosePaginate);
+
+const Quiz = mongoose.model("Quiz", schema);
+export { Quiz };
