15d9baa0484c012027192ff65ee3518c16a218ed
palakcangit <palaksharmas771@gmail.com>
2023-11-18 20:50:31 +0530

    create a function to update status of quiz

---
+++ utills/quizStatusHandler.js
@@ -0,0 +1,29 @@
+import { Quiz } from "../models/Quiz";
+const updateQuizStatus = async () => {
+  try {
+    // Query quizzes that need status update (based on current time)
+    const currentDate = new Date();
+
+    await Quiz.updateMany(
+      {
+        startDate: { $lt: currentDate },
+        endDate: { $gt: currentDate },
+        status: "inactive",
+      },
+      { $set: { status: "active" } }
+    );
+
+    await Quiz.updateMany(
+      { endDate: { $lt: currentDate }, status: "active" },
+      { $set: { status: "finished" } }
+    );
+
+    console.log("Quiz statuses updated successfully.");
+  } catch (error) {
+    console.error("Error updating quiz statuses:", error);
+  } finally {
+    mongoose.disconnect();
+  }
+};
+
+updateQuizStatus();
